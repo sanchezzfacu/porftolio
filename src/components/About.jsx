b2c272df9f3d3b893998a1dc4a4049d4a8b6f360
Facundo Sanchez <sanchezfacundocristian@hotmail.com>
2022-03-23 21:04:49 -0300

    terminado responsive de tablets

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,8 +9,7 @@ function About() {
         <div className='about-container'>
             <h1 className='about-title' ><CircleLogo/>Acerca de mi</h1>
             <div className='img-text-container'>
-                <h2 className='text-about'>Lorem ipsum dolor sit amet consectetur adipisicing elit perspiciatis animi nemo voluptatem consequuntur
-                    unde fugiat ea alias rerum similique ullam sint vel, a, at ex blanditiis corrupti accusamus nobis 
+                <h2 className='text-about'>Lint vel, a, at ex blanditiis corrupti accusamus nobis 
                     laborum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores libero cumque iure magnam, autem
                     maxime! Harum molestias dicta tempore adipisci, cupiditate iure fugit eum ducimus, quasi quisquam alias.
                     Animi, nihil?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure optio labore nobis ipsum libero! 
