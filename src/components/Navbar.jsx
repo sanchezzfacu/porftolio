5a73333c70e082b1bc057ba11a46c36489c9dbee
Facundo Sanchez <sanchezfacundocristian@hotmail.com>
2022-04-21 20:00:10 -0300

    pagina terminada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import { useRef } from 'react'
+// import { useRef } from 'react'
 import Circle from './Circle'
 import '../css/Navbar.css'
+import ScrollIntoView from 'react-scroll-into-view'
 import menu from '../img/menu.png'
-// import CircleLogo from './CircleLogo'
 function Navbar() {
-    const refAbout = useRef(null)// eslint-disable-next-line
-    const executeScroll = () => refAbout.current.scrollIntoView({ behavior: 'smooth', block: 'center' })
-
-    const refProjects = useRef(null)// eslint-disable-next-line
-    const executeScrollProject = () => refProjects.current.scrollIntoView({ behavior: 'smooth', block: 'center' })
-
-    const refContact = useRef(null)// eslint-disable-next-line
-    const executeScrollContact = () => refContact.current.scrollIntoView({ behavior: 'smooth', block: 'center' })
     return (
         <div className='navbar-container'>
             <nav className='nav'>
@@ -22,10 +14,21 @@ function Navbar() {
                     <img className='menu-btn' src={menu} alt="" height='35px'/>
                 </label>
                 <ul>                    
-                    <li><h3 onClick={executeScroll}>About</h3></li>
-                    <li><h3 onClick={executeScrollProject}>Technologies</h3></li>
-                    <li><h3 onClick={executeScrollContact}>Projects</h3></li>
-                    <li><h3 onClick={executeScrollContact}>Contact</h3></li>
+                    <ScrollIntoView selector='.about-container' smooth={true}>
+                        <li><h3>About</h3></li>
+                    </ScrollIntoView>
+
+                    <ScrollIntoView selector='.technologies-container' smooth={true}>
+                        <li><h3>Technologies</h3></li>
+                    </ScrollIntoView>
+
+                    <ScrollIntoView selector='.projects-container' smooth={true} >
+                        <li><h3>Projects</h3></li>
+                    </ScrollIntoView>
+
+                    <ScrollIntoView selector='.form-container' smooth={true}>
+                        <li><h3>Contact</h3></li>
+                    </ScrollIntoView>                    
                 </ul>
             </nav>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,7 +23,7 @@ function Projects() {
                             (comida, tipos de dieta, nivel de salubridad, nombre por orden alfabético) y un formulario para la creación 
                             de nuevas recetas que se almacenan en una base de datos realizada con <b>Sequelize</b> y <b>PostgreSQL</b>.
                         </h3>
-                        <a className='respositorio-link'  href='https://github.com/sanchezzfacu/Proyecto-individual-Foods' target='_blank' rel="noreferrer"><button className='btn-repositorio-left'>Ver código</button></a>
+                        <a className='respositorio-link'  href='https://github.com/sanchezzfacu/Proyecto-individual-Foods' target='_blank' rel="noreferrer"><button className='btn-repositorio-left'>Ver repositorio</button></a>
                     </div>
                     <div className="app-img">
                         <img src={homeFood} alt="imagen no encontrada"/>
@@ -49,7 +49,7 @@ function Projects() {
                                 traves de un formulario controlado y manejo de stock donde se pueden eliminar productos o las cantidades vendidas.
 
                             </h3>
-                            <a className='respositorio-link'  href='https://github.com/sanchezzfacu/mys-accesorios' target='_blank' rel="noreferrer"><button className='btn-repositorio'>Ver código</button></a>
+                            <a className='respositorio-link'  href='https://github.com/sanchezzfacu/mys-accesorios' target='_blank' rel="noreferrer"><button className='btn-repositorio'>Ver repositorio</button></a>
                         </div>
                     </div>
                 </div>
@@ -68,7 +68,7 @@ function Projects() {
                             servidor en <b>ExpressJS</b> y una base de datos en <b>PostgreSQL</b> realizada con <b>Sequelize</b>. Posee una pasarela de pago con 
                             mercado pago, manejo de usuarios, creacion de cuenta con google, carrito de compras, reviews a productos, entre otros.
                         </h3>
-                        <a className='respositorio-link' href='https://github.com/guidoabelleira/E-Commerce-PF10' target='_blank' rel="noreferrer"><button className='btn-repositorio-left'>Ver código</button></a>
+                        <a className='respositorio-link' href='https://github.com/guidoabelleira/E-Commerce-PF10' target='_blank' rel="noreferrer"><button className='btn-repositorio-left'>Ver repositorio</button></a>
                     </div>
                     <div className="app-img">
                         <img src={homeproyectofinal} alt="imagen no encontrada"/>
@@ -78,7 +78,7 @@ function Projects() {
             
             <div className="individual-container">
                 <div className="app-title">
-                    <h1>Breaking Bad App</h1>
+                    <h1>Breaking bad app</h1>
                 </div>
                 <div className="align-right">
                     <div className="app-img">
@@ -93,7 +93,7 @@ function Projects() {
                                 La SPA nos permite crear personajes con su respectivo papel. Se puede usar la barra de búsqueda para buscar personajes,
                                 también se pueden filtrar los personajes. 
                             </h3>
-                            <a className='respositorio-link'  href='https://github.com/sanchezzfacu/Breaking-Bad-App' target='_blank' rel="noreferrer"><button className='btn-repositorio'>Ver código</button></a>
+                            <a className='respositorio-link'  href='https://github.com/sanchezzfacu/Breaking-Bad-App' target='_blank' rel="noreferrer"><button className='btn-repositorio'>Ver repositorio</button></a>
                         </div>
                     </div>
                 </div>
