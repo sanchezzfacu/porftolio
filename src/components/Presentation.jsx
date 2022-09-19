d91303ba1cb3e8d2b113b2e206b348fb095f3353
Facundo Sanchez <sanchezfacundocristian@hotmail.com>
2022-09-19 20:34:17 -0300

    improve css code in presentation secton

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -5,22 +5,22 @@ import '../css/Presentation.css'
 function Presentation() {
     return (
         <div className='presentation-container'>
-            <h4 className='saludo'>Hola, mi nombre es</h4>
-            <h1 className='big-font-presentation'>Facundo Sánchez.</h1>
-            <h2 className='gradient-name big-font-presentation'>Frontend Developer.</h2>
-            <h3 className='presentation-text'><p>Soy un Frontend Developer de Argentina. Un apasionado a desarrollar interfaces 
-                de usuario.</p>
-                <p>
-                    Actualmente me encuentro en búsqueda de mi primer trabajo en IT y me encuentro en constante aprendizaje
-                    para el día que llegue poder darlo todo.
-                </p>
-            </h3>
-            <a href="https://drive.google.com/file/d/1E8Ru30CwXO_QkSNqR72INPdqBN2OyJA_/view" target='_blank' rel='noreferrer'>
-                <button>
-                    Ver Currículum
-                </button>
-            </a>
-            <DecorationCircle/>
+            <div className="presentation-content">
+                <h4 className='greeting'>Hola, mi nombre es</h4>
+                <h1 className='big-font-name'>Facundo Sánchez.</h1>
+                <h2 className='gradient-text big-font-presentation'>Frontend Developer.</h2>
+                <h3 className='presentation-text'>
+                    <p>Soy un Frontend Developer de Argentina. Un apasionado a desarrollar interfaces de usuario.</p>
+                    <p>Actualmente me encuentro en búsqueda de mi primer trabajo en IT y me encuentro en constante aprendizaje
+                        para el día que llegue poder darlo todo.</p>
+                </h3>
+                <a href="https://drive.google.com/file/d/1E8Ru30CwXO_QkSNqR72INPdqBN2OyJA_/view" target='_blank' rel='noreferrer'>
+                    <button>
+                        Ver Currículum
+                    </button>
+                </a>
+                <DecorationCircle/>
+            </div>
         </div>
     )
 }
