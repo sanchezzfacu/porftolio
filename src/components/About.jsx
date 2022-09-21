5e4910976461e3410413f4fffa67a56a789020d8
Facundo Sanchez <sanchezfacundocristian@hotmail.com>
2022-09-21 16:39:08 -0300

    change about and presentation text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,13 +14,18 @@ function About() {
             <div className='img-text-container'>
                 <h2 className='text-about'>
                     <p>
-                        Soy una persona apasionada a la tecnología que disfruta el aprender nuevas
-                        herramientas. Me considero una persona autónoma a la que le encantan los desafios a corto y largo plazo.
+                        Soy un apasionado a la tecnología que disfruta el aprender nuevas herramientas. 
+                         Soy una persona autónoma a la que le encantan los desafios ya que me fuerzan a mejorar y por ende a ser 
+                         un mejor desarrollador.
                     </p>
                     <p>
-                    Disfruto el trabajar con un grupo de personas en el que todos tiramos para el mismo lado. Cuento con buena 
-                    capacidad de analisis y me apasiona todo lo relacionado a crear interfaces de usuarios es por eso que me encuentro 
-                    en constante aprendizaje de nuevas herramientas que me permitan seguir creciendo como desarrollador frontend. 
+                        Me motiva trabajar con un grupo de personas en el que todos tiramos para el mismo lado para conseguir metas 
+                        y objetivos en el corto y largo plazo. 
+                    </p>
+                    <p>
+                        Cuento con buena capacidad de analisis y me apasiona todo lo relacionado
+                        a crear interfaces de usuarios es por eso que me encuentro en constante aprendizaje de nuevas 
+                        herramientas que me permitan seguir creciendo como desarrollador frontend. 
                     </p>
                     <div className='github-linkedin-btn'>
                         <a href='https://www.linkedin.com/in/sanchezzfacu/' target='_blank' rel="noreferrer"><img src={linkedin} alt="" height='40px' /></a>
--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -10,13 +10,18 @@ function Presentation() {
                 <h1 className='big-font-name'>Facundo Sánchez.</h1>
                 <h2 className='gradient-text big-font-presentation'>Frontend Developer.</h2>
                 <h3 className='presentation-text'>
-                    <p>Soy un Frontend Developer de Argentina. Un apasionado a desarrollar interfaces de usuario.</p>
-                    <p>Actualmente me encuentro en búsqueda de mi primer trabajo en IT y me encuentro en constante aprendizaje
-                        para el día que llegue poder darlo todo.</p>
+                    <p>
+                        Soy un Frontend Developer de Argentina apasionado a desarrollar interfaces de usuario.
+                    </p>
+                    
+                    <p>
+                        Actualmente me encuentro en búsqueda de mi primer trabajo en IT y me encuentro en constante aprendizaje
+                        para poder alcanzar ese objetivo.
+                    </p>
                 </h3>
                 <a href="https://drive.google.com/file/d/1E8Ru30CwXO_QkSNqR72INPdqBN2OyJA_/view" target='_blank' rel='noreferrer'>
                     <button>
-                        Ver Currículum
+                        VER CURRICULUM
                     </button>
                 </a>
                 <DecorationCircle/>
