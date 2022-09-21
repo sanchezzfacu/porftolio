8b2b18a0273b4548e2ccb1a984186d9b6961ffb8
Facundo Sanchez <sanchezfacundocristian@hotmail.com>
2022-09-21 20:09:29 -0300

    Âadd animations to presentation

--- src/components/DecorationCircle.jsx
+++ src/components/DecorationCircle.jsx
@@ -1,15 +1,51 @@
 import '../css/DecorationCircle.css'
+import { gsap } from "gsap";
+import { useEffect, useRef } from 'react';
+
 
 function DecorationCircle() {
+    const firstCircle = useRef()
+    const secondCircle = useRef()
+    const thirdCircle = useRef()
+    const fourthCircle = useRef()
+    const fifthCircle = useRef()
+    const sixthCircle = useRef()
+    const seventhCircle = useRef()
+
+    useEffect(() => {
+        gsap.from(firstCircle.current, 4, {
+            opacity: 0,
+        })
+        gsap.from(secondCircle.current, 5, {
+            x: -30,
+            opacity: 0
+        })
+        gsap.from(thirdCircle.current, 2, {
+            opacity: 0
+        })
+        gsap.from(fourthCircle.current, 3, {
+            opacity: 0
+        })
+        gsap.from(fifthCircle.current, 1, {
+            opacity: 0
+        })
+        gsap.from(sixthCircle.current, 5, {
+            opacity: 0
+        })
+        gsap.from(seventhCircle.current, 5, {
+            opacity: 0
+        })
+    })
+
     return (
         <div className='circle-decoration-container'>
-            <div className='first-circle-decoration circle'></div>
-            <div className='second-circle-decoration circle'></div>
-            <div className='third-circle-decoration circle'></div>
-            <div className='fourth-circle-decoration circle'></div>
-            <div className='sixth-circle-decoration circle'></div>
-            <div className='seventh-circle-decoration circle'></div>
-            <div className='eighth-circle-decoration circle'></div>
+            <div className='first-circle-decoration circle' ref={firstCircle}></div>
+            <div className='second-circle-decoration circle' ref={secondCircle}></div>
+            <div className='third-circle-decoration circle' ref={thirdCircle}></div>
+            <div className='fourth-circle-decoration circle' ref={fourthCircle}></div>
+            <div className='sixth-circle-decoration circle' ref={fifthCircle}></div>
+            <div className='seventh-circle-decoration circle' ref={sixthCircle}></div>
+            <div className='eighth-circle-decoration circle' ref={seventhCircle}></div>
         </div>
     )
 }
--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,25 +1,55 @@
-import React from 'react'
+import { gsap } from "gsap";
+import { useEffect, useRef } from 'react';
 import DecorationCircle from './DecorationCircle'
 import '../css/Presentation.css'
 
 function Presentation() {
+    const greeting = useRef()
+    const name = useRef()
+    const gradient = useRef()
+    const presentation = useRef()
+    const btnCv = useRef()
+    
+    useEffect(() => {
+        gsap.from(greeting.current, 3, {
+            opacity: 0,
+            x: -20
+        })
+        gsap.from(name.current, 3, {
+            opacity: 0,
+            x: -20
+        })
+        gsap.from(gradient.current, 3, {
+            opacity: 0,
+            x: -20
+        })
+        gsap.from(presentation.current, 3, {
+            opacity: 0,
+            x: -20
+        })
+        gsap.from(btnCv.current, 3, {
+            opacity: 0,
+            x: -20
+        })
+    })
+
     return (
         <div className='presentation-container'>
             <div className="presentation-content">
-                <h4 className='greeting'>Hola, mi nombre es</h4>
-                <h1 className='big-font-name'>Facundo Sánchez.</h1>
-                <h2 className='gradient-text big-font-presentation'>Frontend Developer.</h2>
-                <h3 className='presentation-text'>
+                <h4 className='greeting' ref={greeting}>Hola, mi nombre es</h4>
+                <h1 className='big-font-name' ref={name}>Facundo Sánchez.</h1>
+                <h2 className='gradient-text big-font-presentation' ref={gradient}>Frontend Developer.</h2>
+                <h3 className='presentation-text' ref={presentation}>
                     <p>
                         Soy un Frontend Developer de Argentina apasionado a desarrollar interfaces de usuario.
                     </p>
-                    
+
                     <p>
                         Actualmente me encuentro en búsqueda de mi primer trabajo en IT y me encuentro en constante aprendizaje
                         para poder alcanzar ese objetivo.
                     </p>
                 </h3>
-                <a href="https://drive.google.com/file/d/1E8Ru30CwXO_QkSNqR72INPdqBN2OyJA_/view" target='_blank' rel='noreferrer'>
+                <a ref={btnCv} href="https://drive.google.com/file/d/1E8Ru30CwXO_QkSNqR72INPdqBN2OyJA_/view" target='_blank' rel='noreferrer'>
                     <button>
                         VER CURRICULUM
                     </button>
